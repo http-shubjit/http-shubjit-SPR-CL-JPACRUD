959b1539707b413550d344dd4d105cb3599078cd
Shubhajit <shubjitgithub@gmail.com>
2024-07-29 11:50:36 +0530

    Done

--- src/main/java/Lab/Service/StoreService.java
+++ src/main/java/Lab/Service/StoreService.java
@@ -20,14 +20,14 @@ public class StoreService {
      * @return the persisted store
      */
     public Store persistStore(Store store){
-        return null;
+        return storeRepository.save(store);
     }
     /**
      * TODO: get all store entities
      * @return all store entities
      */
     public List<Store> getAllStores(){
-        return null;
+        return storeRepository.findAll();
     }
     /**
      * TODO: given an id of a store, return the store.
@@ -35,22 +35,33 @@ public class StoreService {
      * @param id id of store entity
      * @return a store entity
      */
-    public Store getStoreById(long id){
-        return null;
+    public Store getStoreById(long id) {
+        Optional<Store> optional = storeRepository.findById(id);
+        if (optional.isPresent()) {
+            return optional.get();
+        }
+else
+     return null;
     }
     /**
      * TODO: given an id of an existing store, delete the store
      */
     public void deleteStore(long id){
-//        code here
-    }
+        storeRepository.deleteById(id);    }
     /**
      * TODO: given an id and some replacement data for a store, overwrite the data of an existing store,
      * and return the updated store.
      * @return the updated store entity
      */
     public Store updateStore(long id, Store replacement){
-        return null;
+        Optional<Store> optional = storeRepository.findById(id);
+        if (optional.isPresent()) {
+            Store store = optional.get();
+            store.setName(replacement.getName());
+            return storeRepository.save(store);
+        }
+        else
+            return null;
     }
 
 }
